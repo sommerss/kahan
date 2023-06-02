0b89cab72bb0f8330a7a19cc96d1eab1550accb2
disvs <sbsommers3@gmail.com>
2023-06-01 21:23:26 -0700

    cidk

--- main.py
+++ main.py
@@ -5,17 +5,12 @@ from flask import render_template  # import render_template from "public" flask 
 
 # import "packages" from "this" project
 from __init__ import app,db  # Definitions initialization
-from model.jokes import initJokes
-from model.users import initUsers
-from model.players import initPlayers
+
 from model.kahans import initKahans
 
 
 # setup APIs
-from api.covid import covid_api # Blueprint import api definition
-from api.joke import joke_api # Blueprint import api definition
-from api.user import user_api # Blueprint import api definition
-from api.player import player_api
+
 from api.kahan import kahan_api
 
 
@@ -27,10 +22,7 @@ from projects.projects import app_projects # Blueprint directory import projects
 db.init_app(app)
 
 # register URIs
-app.register_blueprint(joke_api) # register api routes
-app.register_blueprint(covid_api) # register api routes
-app.register_blueprint(user_api) # register api routes
-app.register_blueprint(player_api)
+
 app.register_blueprint(kahan_api)
 app.register_blueprint(app_projects) # register app pages
 
@@ -50,9 +42,7 @@ def table():
 
 @app.before_first_request
 def activate_job():  # activate these items 
-    initJokes()
-    initUsers()
-    initPlayers()
+   
     initKahans()
 
 # this runs the application on the development server
